46402cf538d667b0eb8eb29d4c6cad8c3af18e40
Teng M <davebryan.sevilla@g.batstate-u.edu.ph>
2022-02-07 09:39:20 +0800

    changes

--- seller/merchant_signup.php
+++ seller/merchant_signup.php
@@ -14,7 +14,7 @@ require_once '../connection.php';
         <script src="https://kit.fontawesome.com/dbed6b6114.js" crossorigin="anonymous"></script>
         <link rel = "icon" href = "images/logo.png" type = "image/png">
     </head>
-    <body style="background-image:url('../img/bg.jpg');background-size:100% 100%;background-repeat:no-repeat;background-attachment:fixed">
+    <body >
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>  
    
 <body>
