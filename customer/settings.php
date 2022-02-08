9c60f15b4c24e93a05f850f1f449076c9d3c8c73
Teng M <davebryan.sevilla@g.batstate-u.edu.ph>
2022-02-09 07:55:16 +0800

    changes

--- customer/settings.php
+++ customer/settings.php
@@ -9,19 +9,140 @@ require '../connection.php';
     <head>
         <meta charset="utf-8">
         <title>Settings</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">    
         <script src="https://kit.fontawesome.com/dbed6b6114.js" crossorigin="anonymous"></script>
-        <link rel = "icon" href = "images/logo.png" type = "image/png">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="../main.css">
+  
     </head>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> 
 
-
-    <body>
+    <body style="background-color:white;">
         
       <!-- Navbar-->
       <?php include 'navbar.php' ?>
-       
+  
+      <p class="text-center h2 fw-bold mb-3 mx-1 mx-md-4 mt-4"  
+      style="color:#0073ae;text-shadow: 1px 1px #03a9f4;">Edit Account
+      </p>
 
+  
+<div class="vh-100" >
+  <div class="container h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-lg-12 col-xl-11">
+        <div class="card text-black" style="border-radius: 25px;">
+          <div class="card-body p-md-5">
+            <div class="row justify-content-center">
+              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+
+                <p class="text-left h4 fw-bold mb-3 mx-1 mx-md-4 mt-4">Personal Information</p>
+
+                <form class="mx-1 mx-md-4" action="" method="POST" enctype="multipart/form-data">
+
+                
+                <div class="d-flex flex-row align-items-center mb-2">
+                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="text" name="username" id="form3Example1c" class="form-control" placeholder="Username" />
+  
+                    </div>
+                  </div>
+
+
+                  <div class="d-flex flex-row align-items-center mb-2">
+                    <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="email" name="email" id="form3Example3c" class="form-control" placeholder="Email"/>
+
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-2">
+                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="text" name="first_name" id="form3Example3c" class="form-control" placeholder="First Name"/>
+
+                    </div>
+                  </div>
+
+
+                  <div class="d-flex flex-row align-items-center mb-2">
+                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="text" name="last_name" id="form3Example3c" class="form-control" placeholder="Last Name"/>
+
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-2">
+                    <i class="fas fa-map-marker fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="text" name="address" id="form3Example3c" class="form-control" placeholder="Address"/>
+          
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-2">
+                    <i class="fas fa-phone fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="text" name="contact_number" id="form3Example4cd" class="form-control" placeholder="Phone" />
+      
+                    </div>
+                  </div>
+
+<br><hr style="color:black;">
+<p class="text-left h4 fw-bold mb-3 mx-1 mx-md-4 mt-4">Change Password</p>
+
+                  <div class="d-flex flex-row align-items-center mb-2">
+                    <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="password" name="password" id="form3Example4c" class="form-control" placeholder="Password"/>
+
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-2">
+                    <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="password" name="password" id="form3Example4c" class="form-control" placeholder="Password"/>
+
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-2">
+                    <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="password" name="password" id="form3Example4c" class="form-control" placeholder="Password"/>
+
+                    </div>
+                  </div>
+
+                  <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                    <button style="width:250px;" type="submit" name="submit" class="btn btn-primary btn-lg">Save Changes</button>
+                  </div>
+                </form>
+
+              </div>
+              <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+
+                <img src="../img/accset.webp" class="img-fluid" alt="Sample image">
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+                 
+
+
+            
+        
+      
+      
 
 
 
