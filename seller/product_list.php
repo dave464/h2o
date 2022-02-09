37914be3bcc438d95083fdb91e9a05fb5e286855
Teng M <davebryan.sevilla@g.batstate-u.edu.ph>
2022-02-10 00:04:52 +0800

    changes

--- seller/product_list.php
+++ seller/product_list.php
@@ -10,16 +10,18 @@ include '../connection.php';
         <meta charset="utf-8">
         <title>Index</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="main.css">
+        <link rel="stylesheet" href="../main.css">
         <script src="https://kit.fontawesome.com/dbed6b6114.js" crossorigin="anonymous"></script>
-        <link rel = "icon" href = "images/logo.png" type = "image/png">
-    </head>
-    <body style="background-image:url('../img/bg.jpg');background-size:100% 100%;background-repeat:no-repeat;background-attachment:fixed">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         
-      <!-- Navbar-->
+    </head>
+    <body >
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> 
+      
+    <!-- Navbar-->
       <?php include 'navbar.php' ?>
        
-
+<!--
        <center>
              <div class="content">
                 
@@ -48,163 +50,69 @@ include '../connection.php';
 
             </div>
 
+      -->
+
+<!--------- SECTION Start-------->
+<section style="background-color: #eee;">
+  <div class="container py-5">
+    <div class="row">
+
+        
+<?php  
+   $query = $conn->query("SELECT product.product_id,product.image,product.product_name,
+   product.price, merchant.merchant_id , merchant.business_name
+              FROM merchant RIGHT JOIN product ON merchant.merchant_id = product.merchant_id WHERE  product.merchant_id = '".$_SESSION['merchant_id']."'") or die(mysqli_error());
+              while($fetch = $query->fetch_array()){
+ ?>
+
+
+      <div class="col-md-12 col-lg-4 mb-4 mb-lg-0">
+        <div class="card">
+          <div class="d-flex justify-content-between p-3">
+            <p class="lead mb-0"><?php echo $fetch['business_name']?>'s Offer</p>
+            <a href="#!">
+              <div class="mask">
+                <div class="d-flex justify-content-start align-items-end h-100">
+                  <h2><span class="badge bg-warning ms-2">Sale</span></h2>
+                </div>
+              </div>
+              <div class="hover-overlay">
+                <div
+                  class="mask"
+                  style="background-color: rgba(251, 251, 251, 0.15);"
+                ></div>
+              </div>
+            </a>
+          </div>
+          
+          <img src = "../photo/<?php echo $fetch['image']?>"  class="card-img-top"/>
+          <div class="card-body">
+            
+
+            <div class="d-flex justify-content-between mb-3">
+              <h5 class="mb-0"><?php echo $fetch['product_name']?></h5>
+              <h5 class="text-dark mb-0">&#8369; <?php echo $fetch['price']?>.00</h5>
+            </div>
+
+           
+          </div>
+        </div>
+      </div>
+            
+ <?php
+       }
+    ?>
+
+     
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!--------- SECTION END-------->
 
   
 
     </body>
 </html>
 
-
-<style>
-*{
-  color:white;
-}
-
-h2 {
-    background-color: #ADD8E6;
-    padding:13px;
-    color:#000;
-    font-size:19px;
-    margin-right: 23px;
-     
-    margin-left:23px;
-}
-.myButton {
-width:95%;
-    opacity:0.8;
-    -moz-box-shadow:inset 0px 1px 0px 0px #cf866c;
-    -webkit-box-shadow:inset 0px 1px 0px 0px #cf866c;
-    box-shadow:inset 0px 1px 0px 0px #cf866c;
-    background-color:#FFF;
-    -moz-border-radius:3px;
-    -webkit-border-radius:3px;
-    border-radius:3px;
-    border:1px solid #942911;
-    display:inline-block;
-    cursor:pointer;
-    color:#000000;
-    font-family:Arial;
-    font-size:15px;
-    padding:15px;
-    text-decoration:none;
-    text-shadow:0px 1px 0px #854629;
-}
-
-
-@import url(https://fonts.googleapis.com/css?family=Open+Sans+Condensed:700);
-
-html  {
-color:#000;
-height:100%;
-}
-body {
-  
-  font-family: "Open Sans Condensed", sans-serif;
-}
-
-#bg {
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  background: url(../img/bg.jpeg) no-repeat center center fixed;
-  background-size: cover;
-  -webkit-filter: blur(5px);    
-}
-
-form {
-  position: relative;
-  width: 90%;
-  height:100%;
-  margin: 0 auto;
-  background: rgba(130,130,130,.3);
-  padding: 20px 22px;
-  border: 1px solid;
-  border-top-color: rgba(255,255,255,.4);
-  border-left-color: rgba(255,255,255,.4);
-  border-bottom-color: rgba(60,60,60,.4);
-  border-right-color: rgba(60,60,60,.4);
-  color: white;
-}
-
-form input, form select, form textarea, form button {
-  width: 87%;
-  border: 1px solid;
-  border-bottom-color: rgba(255,255,255,.5);
-  border-right-color: rgba(60,60,60,.35);
-  border-top-color: rgba(60,60,60,.35);
-  border-left-color: rgba(80,80,80,.45);
-  background-color: rgba(0,0,0,.2);
-  background-repeat: no-repeat;
-  padding: 8px 24px 8px 10px;
-  font: bold .875em/1.25em "Open Sans Condensed", sans-serif;
-  letter-spacing: .075em;
-  color: #fff;
-  text-shadow: 0 1px 0 rgba(0,0,0,.1);
-  margin-bottom: 19px;
-}
-
-form input:focus { background-color: rgba(0,0,0,.4); }
-
-
-
-::-webkit-input-placeholder { color: #ccc; text-transform: uppercase; }
-::-moz-placeholder { color: #ccc; text-transform: uppercase; }
-:-ms-input-placeholder { color: #ccc; text-transform: uppercase; }
-
-form button[type=submit] {
-  width: 248px;
-  margin-bottom: 0;
-  color: #3f898a;
-  letter-spacing: .05em;
-  text-shadow: 0 1px 0 #133d3e;
-  text-transform: uppercase;
-  background: #225556;
-  border-top-color: #9fb5b5;
-  border-left-color: #608586;
-  border-bottom-color: #1b4849;
-  border-right-color: #1e4d4e;
-  cursor: pointer;
-}
-form button {
-  width: 248px;
-  margin-bottom: 0;
-  color: #3f898a;
-  letter-spacing: .05em;
-  text-shadow: 0 1px 0 #133d3e;
-  text-transform: uppercase;
-  background: #225556;
-  border-top-color: #9fb5b5;
-  border-left-color: #608586;
-  border-bottom-color: #1b4849;
-  border-right-color: #1e4d4e;
-  cursor: pointer;
-}
-p {
-color:#FFF;
-}
-h1 {
-color:#FFF;
-
-}
-
-.dropdown {
-  position: relative;
-  display: inline-block;
-}
-
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #f9f9f9;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  padding: 12px 16px;
-  z-index: 1;
-}
-
-.dropdown:hover .dropdown-content {
-  display: block;
-}
-</style>
