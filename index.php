cb15b8c799507c6b1adb0fc218b23c79bc6dc523
Teng M <davebryan.sevilla@g.batstate-u.edu.ph>
2022-02-09 08:06:18 +0800

    changes

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         <script src="https://kit.fontawesome.com/dbed6b6114.js" crossorigin="anonymous"></script>
         <link rel = "icon" href = "images/logo.png" type = "image/png">
     </head>
-    <body style="background-image:url('img/bg.jpg');background-size:100% 100%;background-repeat:no-repeat;background-attachment:fixed">
+    <body >
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 <center><img src="img/DM.gif" style=" width: 400px; height: 400px;"></center>
