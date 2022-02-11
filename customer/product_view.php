f6d8494691baaaa92a835777527f8a74036b49c8
Teng M <davebryan.sevilla@g.batstate-u.edu.ph>
2022-02-11 11:06:24 +0800

    changes

--- customer/product_view.php
+++ customer/product_view.php
@@ -9,78 +9,106 @@ require '../connection.php';
     <head>
         <meta charset="utf-8">
         <title>Product</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="main.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../main.css">
         <script src="https://kit.fontawesome.com/dbed6b6114.js" crossorigin="anonymous"></script>
-        <link rel = "icon" href = "images/logo.png" type = "image/png">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/js/all.min.js" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        
     </head>
 
-    <body  style="background-image:url('../img/bg.jpg');background-size:100% 100%;background-repeat:no-repeat;background-attachment:fixed" >
-        
+    <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>  
      
+    <!-- Navbar-->
+    <?php include 'navbar.php' ?>
+   
+    <?php
+      $query = $conn->query("SELECT * FROM `product` WHERE `product_id` = '$_REQUEST[product_id]'") or die(mysqli_error());
+      $fetch = $query->fetch_array();
+    ?>
        
-
-<center>
-
-<?php
-  $query = $conn->query("SELECT * FROM `product` WHERE `product_id` = '$_REQUEST[product_id]'") or die(mysqli_error());
-  $fetch = $query->fetch_array();
-?>
-
-           <div class="content">
-          <table>
-        
-        
-        </table>
+       <p class="text-center h1 fw-bold mb-3 mx-1 mx-md-4 mt-4"  
+       style="color:#0073ae;text-shadow: 1px 1px #03a9f4;"><?php echo $fetch['product_name']?> - &#8369; <?php echo $fetch['price']?>.00
+       </p>   
 
 
+<!--------- SECTION Start-------->
+<center><section  style="margin-top:-30px;">
+  <div class="container py-5">
 
-<h2 style="background-color: #ADD8E6;;color:#000;"><?php echo $fetch['product_name']?> -  &#8369; <?php echo $fetch['price']?> </h2> 
-<br>
 
+    <?php  
+      $query = $conn->query("SELECT product.product_id,product.image,product.product_name,
+      product.price,product.product_type, merchant.merchant_id, merchant.business_name
+      FROM merchant RIGHT JOIN product ON merchant.merchant_id = product.merchant_id WHERE  product_id = '".$_REQUEST['product_id']."'") or die(mysqli_error());
+    ?>
 
-<?php  
-   $query = $conn->query("SELECT product.product_id,product.image,product.product_name,product.product_type,
-   product.price,product.description, merchant.merchant_id
-              FROM merchant RIGHT JOIN product ON merchant.merchant_id = product.merchant_id WHERE  product_id = '".$_REQUEST['product_id']."'") or die(mysqli_error());
-             
- ?>
- <form action="addcart.php" method="POST" enctype="multipart/form-data" >
+<form action="addcart.php" method="POST" enctype="multipart/form-data" >
    <input type="hidden" value="<?php echo $fetch['product_id']?>" name="product_id">
    <input type="hidden" value="<?php echo $_SESSION['customer_id']?>" name="customer_id">
-      <div style="border:1px solid #FFF;width:98%;margin:5px;float:left;background-color:#FFF;padding-bottom:50px"><center>
-        <div class="slideshow-container">
-          <div class="mySlides fade">  
-            <img src="../photo/<?php echo $fetch['image']?>" style="width:100%;height:350px" onclick="window.location='../photo/<?php echo $fetch['image']?>'">
-        </div>
-            <a class="prev" onclick="plusSlides(-1)" style="left:0">&#10094;</a>
-            <a class="next" onclick="plusSlides(1)">&#10095;</a></div></center>
-            <h3 style="color:#000;margin:5px;margin-left:15px"><?php echo $fetch['product_name']?></h3>
-            <h3 style="color:#000;margin:5px;margin-left:15px">  &#8369; <?php echo $fetch['price']?></h3>
-            <h3 style="color:#000;margin:5px;margin-left:15px">Type: <?php echo $fetch['product_type']?></h3><hr style="width:90%">
-            <h3 style="color:#000;margin:5px;margin-left:15px">Description: <?php echo $fetch['description']?></h3>
+          
+   
+      <div class="col-md-12 col-lg-4 mb-4 mb-lg-0">
+        <div class="card">
+          
+          <img src = "../photo/<?php echo $fetch['image']?>" 
+          onclick="window.location='product_view.php?product_id=<?php echo $fetch['product_id']?>'"
+           class="card-img-top"/>
+           
+          <div class="card-body">
+          
+            <div class="d-flex justify-content-between">
+              <p class="small"><?php echo $fetch['product_type']?></p>
+             
+            </div>
+                
+
+            <div class="d-flex justify-content-between mb-3">
+              <h5 class="mb-0"><?php echo $fetch['product_name']?></h5>
+              <h5 class="text-dark mb-0">&#8369; <?php echo $fetch['price']?>.00</h5>
+            </div>
+
+            <hr style="background-color: black">
+           <!--  <div class="d-flex justify-content-between mb-2">
+              <p class="text-muted mb-0">Rating: </p>
+             <div class="ms-auto text-warning">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+              </div>
+            </div>-->
+
+            <div class="d-flex justify-content-between mb-3">
+              <h5 class="mb-0">Description: <?php echo $fetch['description']?></h5>
+              
+            </div>
+
+            <div class="d-flex justify-content-center align-items-center  mb-3">
+              
+            <center><br>Enter Quantity:<br><span style="font-size:100%;color:#CCCC00;margin:20px">
+            <input type="number" name="number_of_items" class="email" id="number" min="1" pattern="[1-9]*"
+            style="text-align:center"></span></center>
+              
+            </div>
             
-            <center><br>Ratings:<br><span style="font-size:200%;color:#CCCC00;margin:20px">&starf;</span></center>
-            <center><br>Enter Quantity:<br><span style="font-size:200%;color:#CCCC00;margin:20px">
-            <input type="number" name="number_of_items" class="email" id="number" style="text-align:center"></span></center>
-            <center><br><button type="submit" name="submit" onclick="addcart();" class="myButton" style="color:#000">Add to Cart</button><br><br>
-
-              <!-- Rate Product<br>
-
-          <select name="rate" id="rate" class="email">
-            <option></option>
-            <option value="1"><span style="font-size:200%;color:#CCCC00;margin:20px">&starf;</span></option>
-            <option value="2"><span style="font-size:200%;color:#CCCC00;margin:20px">&starf;&starf;</span></option>
-          <option value="3"><span style="font-size:200%;color:#CCCC00;margin:20px">&starf;&starf;&starf;</span></option>
-          <option value="4"><span style="font-size:200%;color:#CCCC00;margin:20px">&starf;&starf;&starf;&starf;</span></option>
-          <option value="5"><span style="font-size:200%;color:#CCCC00;margin:20px">&starf;&starf;&starf;&starf;&starf;</span></option>
-        </select>
-        <center><br><a onclick="rateproduct();" class="myButton" style="color:#000">Rate Product</a></div> -->
-</form>
-
-       
+            <center><br><button type="submit" name="submit" onclick="addcart();" class="myButton" 
+            style="color:#000"><i class="fas fa-cart-plus"></i> Add to Cart</button><br><br>
+          
+          </div>
+        </div>
+      </div>
+            
  
-
+     
+        </div>
+      </div>
+      </form>
+  </div>
+</section>
+<!--------- SECTION END-------->    
 
    <script>
     function addcart() {
@@ -88,6 +116,10 @@ require '../connection.php';
       
       if(number=="") {
         alert("Please enter quantity first");
+      }
+
+      if(number<="0") {
+        alert("Value be must greater than or equal to 1");
       }
 
       
@@ -141,11 +173,7 @@ function showSlides(n) {
 }
 </script>
     
-      </div>
-
-
-
-
+    
 
     </body>
 </html>
@@ -153,112 +181,11 @@ function showSlides(n) {
 
 
 <style>
-* {box-sizing: border-box; color: black}
-body {font-family: Verdana, sans-serif; margin:0}
-.mySlides {display: none}
-img {vertical-align: middle;}
+.card{
+  box-shadow: 13px 13px 20px #cbced1, -13px -13px 20px #fff;
 
-/* Slideshow container */
-.slideshow-container {
-  max-width: 1000px;
-  position: relative;
-  margin: auto;
 }
 
-/* Next & previous buttons */
-.prev, .next {
-  cursor: pointer;
-  position: absolute;
-  top: 50%;
-  width: auto;
-  padding: 16px;
-  margin-top: -22px;
-  color: white;
-  font-weight: bold;
-  font-size: 18px;
-  transition: 0.6s ease;
-  border-radius: 0 3px 3px 0;
-  user-select: none;
-}
-
-/* Position the "next button" to the right */
-.next {
-  right: 0;
-  border-radius: 3px 0 0 3px;
-}
-
-/* On hover, add a black background color with a little bit see-through */
-.prev:hover, .next:hover {
-  background-color: rgba(0,0,0,0.8);
-}
-
-/* Caption text */
-.text {
-  color: #f2f2f2;
-  font-size: 15px;
-  padding: 8px 12px;
-  position: absolute;
-  bottom: 8px;
-  width: 100%;
-  text-align: center;
-}
-
-/* Number text (1/3 etc) */
-.numbertext {
-  color: #f2f2f2;
-  font-size: 12px;
-  padding: 8px 12px;
-  position: absolute;
-  top: 0;
-}
-
-/* The dots/bullets/indicators */
-.dot {
-  cursor: pointer;
-  height: 15px;
-  width: 15px;
-  margin: 0 2px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
-  transition: background-color 0.6s ease;
-}
-
-.active, .dot:hover {
-  background-color: #717171;
-}
-
-/* Fading animation */
-.fade {
-  -webkit-animation-name: fade;
-  -webkit-animation-duration: 1.5s;
-  animation-name: fade;
-  animation-duration: 1.5s;
-}
-
-@-webkit-keyframes fade {
-  from {opacity: .4} 
-  to {opacity: 1}
-}
-
-@keyframes fade {
-  from {opacity: .4} 
-  to {opacity: 1}
-}
-
-/* On smaller screens, decrease text size */
-@media only screen and (max-width: 300px) {
-  .prev, .next,.text {font-size: 11px}
-}
-</style>
-
-
-<style>
-h2 {
-  background-color: #ADD8E6;;
-  padding:15px;
-  color:#000;
-}
 .myButton {
   box-shadow:inset 0px 1px 0px 0px #fff6af;
   background:linear-gradient(to bottom, #ffec64 5%, #ffab23 100%);
@@ -282,117 +209,5 @@ h2 {
 .myButton:active {
   position:relative;
   top:1px;
-}
-
-
-@import url(https://fonts.googleapis.com/css?family=Open+Sans+Condensed:700);
-
-html, body,form  {
-color:#000;
-height:100%;
-}
-body {
-  background: #999;
-  
-  padding: 20px;
-  font-family: "Open Sans Condensed", sans-serif;
-}
-
-#bg {
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  background: url(img/bg.jpg) no-repeat center center fixed;
-  background-size: cover;
-  -webkit-filter: blur(5px);    
-}
-
-/* form input, form button {
-  width: 87%;
-  border: 1px solid;
-  border-bottom-color: rgba(255,255,255,.5);
-  border-right-color: rgba(60,60,60,.35);
-  border-top-color: rgba(60,60,60,.35);
-  border-left-color: rgba(80,80,80,.45);
-  background-color: rgba(0,0,0,.2);
-  background-repeat: no-repeat;
-  padding: 8px 24px 8px 10px;
-  font: bold .875em/1.25em "Open Sans Condensed", sans-serif;
-  letter-spacing: .075em;
-  color: #fff;
-  text-shadow: 0 1px 0 rgba(0,0,0,.1);
-  margin-bottom: 19px;
-}
-
-form input:focus { background-color: rgba(0,0,0,.4); }
-
-
-
-::-webkit-input-placeholder { color: #ccc; text-transform: uppercase; }
-::-moz-placeholder { color: #ccc; text-transform: uppercase; }
-:-ms-input-placeholder { color: #ccc; text-transform: uppercase; }
-
-form button[type=submit] {
-  width: 248px;
-  margin-bottom: 0;
-  color: #3f898a;
-  letter-spacing: .05em;
-  text-shadow: 0 1px 0 #133d3e;
-  text-transform: uppercase;
-  background: #225556;
-  border-top-color: #9fb5b5;
-  border-left-color: #608586;
-  border-bottom-color: #1b4849;
-  border-right-color: #1e4d4e;
-  cursor: pointer;
-}
-form button {
-  width: 248px;
-  margin-bottom: 0;
-  color: #3f898a;
-  letter-spacing: .05em;
-  text-shadow: 0 1px 0 #133d3e;
-  text-transform: uppercase;
-  background: #225556;
-  border-top-color: #9fb5b5;
-  border-left-color: #608586;
-  border-bottom-color: #1b4849;
-  border-right-color: #1e4d4e;
-  cursor: pointer;
-} */
-p {
-color:#FFF;
-}
-h1 {
-color:#FFF;
-
-}
-
-.dropdown {
-  position: relative;
-  display: inline-block;
-}
-
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #f9f9f9;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  padding: 12px 16px;
-  z-index: 1;
-}
-
-.dropdown:hover .dropdown-content {
-  display: block;
-}
-
-.email {
-  width: 40%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  box-sizing: border-box;
 }
 </style>
