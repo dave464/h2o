d54775c56841fc951551adc514f237227595f733
Teng M <davebryan.sevilla@g.batstate-u.edu.ph>
2022-06-07 08:39:26 +0800

    changes

--- customer/delivery_destination.php
+++ customer/delivery_destination.php
@@ -14,15 +14,21 @@ require_once '../connection.php';
     <meta charset="utf-8" />
     <title>Delivery Destination</title>
    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css" />
-    <link rel="stylesheet" href="../dist/leaflet-routing-machine.css" />
-    <link rel="stylesheet" href="index.css" />
+   
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/leaflet.css" />
+    <link rel="stylesheet" href="../deliveryman/index.css" />
+
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/leaflet.js"></script>
+        <script src="https://www.mapquestapi.com/sdk/leaflet/v2.2/mq-map.js?key=1CWxNVqNiLCgLCpsMq2tobfcQbiMGrRG"></script>
+        <script src="https://www.mapquestapi.com/sdk/leaflet/v2.2/mq-routing.js?key=1CWxNVqNiLCgLCpsMq2tobfcQbiMGrRG"></script>
 </head>
 <body style='border:0; margin: 0'>
-       <div id="map" class="map"></div>
-    <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js"></script>
-     <script src="../dist/leaflet-routing-machine.js"></script>
-    <script src="Control.Geocoder.js"></script>
+       <div id="map" class="map">
+          
+       </div>
+
+    
 
 
  <?php
@@ -32,7 +38,19 @@ require_once '../connection.php';
             $query = $conn->query("SELECT product.product_id,product.image,product.product_name,product.product_type,
             product.price, product.merchant_id,orderlist.status, orderlist.order_id,orderlist.quantity,
             orderlist.total, orderlist.type, orderlist.photo,orderlist.date, merchant.business_name,merchant.merchant_id
-            ,customer.firstname, customer.lastname, customer.address, customer.contact_number,customer.customer_id,customer.c_latitude,customer.c_longitude ,deliveryman.d_longitude,deliveryman.d_latitude
+            ,customer.firstname, customer.lastname, customer.address, customer.contact_number,customer.customer_id,customer.c_latitude,customer.c_longitude ,deliveryman.d_longitude ,deliveryman.d_latitude,deliveryman.deliveryman_id,
+            
+             (
+      3959  * acos (
+      cos ( radians(customer.c_latitude) )
+      * cos( radians(deliveryman.d_latitude) )
+      * cos( radians(deliveryman.d_longitude) - radians(customer.c_longitude) )
+      + sin ( radians(customer.c_latitude) )
+      * sin( radians(deliveryman.d_latitude) )
+    )
+) AS distance
+
+
             FROM `orderlist`
             RIGHT JOIN product ON orderlist.product_id = product.product_id
             RIGHT JOIN merchant ON orderlist.merchant_id = merchant.merchant_id
@@ -51,34 +69,52 @@ require_once '../connection.php';
             <input type="hidden" value="<?php echo $fetch['quantity']?>" name="quantity">
             <input type="hidden" value="<?php echo $fetch['quantity'] * $fetch['price']?>" name="total">
             <input type="hidden" value="<?php echo $fetch['order_id']?>" name="order_id">
- <input type="hidden" value="<?php echo $fetch['deliveryman_id']?>" name="deliveryman_id">
+             <input type="hidden" value="<?php echo $fetch['deliveryman_id']?>" name="deliveryman_id">
 
 
 <input type="hidden" value="<?php echo $fetch['product_id']?>" name="product_id">
           <input type="hidden" value="<?php echo $_SESSION['customer_id']?>" name="customer_id">
           <input type="hidden" value="<?php echo $fetch['merchant_id']?>" name="merchant_id">
 
-<!--<div class="formBlock">
-    <form id="form">-->
+<div class="formBlock">
+    <form id="form">
            <input type="hidden" name="c_latitude" class="input"  value="<?php echo $fetch['c_latitude']?>" id="c_latitude" ><br>
      <input type="hidden" name="c_longitude" class="input"  value="<?php echo $fetch['c_longitude']?>" id="c_longitude" ><br>
 
         <input type="hidden" name="d_latitude" class="input"  value="<?php echo $fetch['d_latitude']?>" id="d_latitude" ><br>
      <input type="hidden" name="d_longitude" class="input"  value="<?php echo $fetch['d_longitude']?>" id="d_longitude" ><br>
 
-    <!--</form>
-</div>-->
+     <input type="type"  class="input"  value=" <?php 
 
-     <?php if($del_lati== null && $del_long ==null) {
-                
+     include 'ReverseLatlong.php';
 
-  echo ("<script>
-    alert('Please Update your Pin Location');
-    document.location.href = 'dispatched_order_details.php?order_id=".$fetch['order_id']."';
-    </script>");
 
-              } ?>
-             
+print_r (LatLongAddres(''.$fetch['d_latitude'].'',''.$fetch['d_longitude'].''));
+
+     ?>" id="AD" ><br>
+
+<input type="type" name="distance" class="input"  value="<?php 
+
+              $x=$fetch['distance']*1.609344;
+              $y=1000;
+
+             if ( $fetch['distance'] > 1 ) {
+                echo round('  '.$fetch['distance'].' ) ',2);
+                echo " KM";
+
+             }elseif ($fetch['distance'] < 1) {
+                echo  round($x * $y,2);
+                echo " M";
+             }
+         
+         ?>" id="DIS" ><br>
+    
+
+    </form>
+</div>
+
+
+
 
         </div>
  
@@ -95,7 +131,16 @@ require_once '../connection.php';
 
     
 <script type="text/javascript">
- var map = L.map('map');
+
+
+    let lattii = document.getElementById("d_latitude").value; 
+    let longii = document.getElementById("d_longitude").value;
+
+ var map = L.map('map', {
+    layers: MQ.mapLayer(),
+    center: [lattii,longii],
+    zoom: 18
+});
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const tileLayer = L.tileLayer(tileUrl,{
  maxZoom: 21 ,
@@ -118,85 +163,44 @@ function showPosition(position) {
 
     let latti = document.getElementById("d_latitude").value; 
     let longi = document.getElementById("d_longitude").value;
+    let clatti = document.getElementById("c_latitude").value; 
+    let clongi = document.getElementById("c_longitude").value;
+    let ADD=document.getElementById("AD").value;
+    let Dis=document.getElementById("DIS").value;
 
-    let latti2= document.getElementById("c_latitude").value;
-    let longi2= document.getElementById("c_longitude").value;
 
+    var g1 = L.icon({
+    iconUrl: '../img/marker.png',
+    iconSize: [30, 30], // size of the icon
+     });
+
+    L.marker([latti,longi],{icon: g1}).addTo(map)
+    .bindPopup(' <center> <img src = "../img/marker.png" style="width: 100px;height: 110px; margin-bottom: 5px;" /> </center>   '
+     + '<br>'+'<b>Location:</b>'+ ADD + '<br>'+ '<b>Distance:</b> '+ Dis ).openPopup();
     
-    var control = L.Routing.control({
-    waypoints: [
-        L.latLng(latti,longi),
-        L.latLng(latti2, longi2)
-    ],
-    
-    routeWhileDragging: true,
-    reverseWaypoints: true,
-    showAlternatives: true,
-    altLineOptions: {
-        styles: [
-            {color: 'black', opacity: 0.15, weight: 9},
-            {color: 'white', opacity: 0.8, weight: 6},
-            {color: 'blue', opacity: 0.5, weight: 2}
-        ]
-    },
 
-createMarker: function (i, waypoint, n) {
+    var g2 = L.icon({
+    iconUrl: 'end.png',
+    iconSize: [25, 25], // size of the icon
+      });
 
+    L.marker([clatti,clongi],{icon: g2}).addTo(map)
 
-let urlIcon;
-
-if(i==0){
-    urlIcon=('../img/marker.png');
-
-}
-else if((i+1)==n){
-    urlIcon=('end.png');
-
-}
-else{
-    urlIcon=('foot.png');
-
-}
-
-            const marker = L.marker(waypoint.latLng, {
-              
-              bounceOnAdd: false,
-              bounceOnAddOptions: {
-                duration: 1000,
-                height: 800,
-                function() {
-                  (bindPopup(myPopup).openOn(map))
-                }
-              },
-              icon: L.icon({
-                iconUrl: urlIcon,
-                iconSize: [35, 35]
-              })
-            });
-            return marker;
-          
-
-}
-
-
-}).addTo(map);
-
-L.Routing.errorControl(control).addTo(map);
-}
+   }
 
 getLocation();
 
 </script>
 
-
-<!--<style type="text/css">
- .formBlock {
-    max-width: 60px;
+<!--
+<style type="text/css">
+  .formBlock {
+    max-width: 200px;
     background-color: #FFF;
     border: 1px solid #ddd;
     position: absolute;
-    top: 90px;
-    left: 5px;
+    top: 40em;
+    left:5px;
     padding: 8px;
     z-index: 999;
     box-shadow: 0 1px 5px rgba(0,0,0,0.65);
@@ -219,8 +223,7 @@ getLocation();
 input:nth-child(1) {
     margin-bottom: 10px;
 }
-</style>-->
-
+-->
 
 </body>
 </html>
